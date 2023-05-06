005dc191b466b24b1ccc82bdcebf221fd8913c1c
yaroslav <yaroslav.bakhmach@suntechinnovation.com>
2023-05-06 12:36:36 +0300

    home sales

--- home-sales/src/app/app.module.ts
+++ home-sales/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { HeaderComponent } from './header/header.component';
+import { MapContainerComponent } from './map-container/map-container.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    HeaderComponent,
+    MapContainerComponent
   ],
   imports: [
     BrowserModule,
---
+++ home-sales/src/app/map-container/map-container.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-map-container',
+  templateUrl: './map-container.component.html',
+  styleUrls: ['./map-container.component.scss'],
+  standalone: true
+})
+export class MapContainerComponent {
+
+}

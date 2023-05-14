f2bd18a041441087de61c2ed4aa49bd578b1bf54
yaroslav <yaroslav.bakhmach@suntechinnovation.com>
2023-05-14 15:11:57 +0300

    added test config for mapbox and layers

--- home-sales/src/app/map-container/map-container.component.ts
+++ home-sales/src/app/map-container/map-container.component.ts
@@ -2,6 +2,14 @@ import {Component, OnInit, ViewEncapsulation} from '@angular/core';
 import * as mapboxgl from "mapbox-gl";
 import {environment} from "../../enironment/environment";
 import {AttributionControl, CircleLayer, GeolocateControl, NavigationControl, ScaleControl} from "mapbox-gl";
+import {
+  CompassControl,
+  InspectControl,
+  LanguageControl,
+  RulerControl,
+  StylesControl,
+  ZoomControl
+} from "mapbox-gl-controls";
 
 @Component({
   selector: 'app-map-container',
@@ -48,11 +56,70 @@ export class MapContainerComponent implements OnInit {
     this.map = new mapboxgl.Map({
       container: 'map',
       style: this.style,
-      zoom: 13,
+      zoom: 10,
       center: [this.lng, this.lat],
       accessToken: environment.mapbox.accessToken,
-      scrollZoom: true
     })
+
+    this.map.on('load', () => {
+      this.map.addSource('point', {
+        type: 'geojson',
+        data: {
+          type: 'FeatureCollection',
+          features: [{
+            type: 'Feature',
+            geometry: {
+              type: 'Point',
+              coordinates: [30.3661256, 50.429972]
+            },
+            properties: {}
+          }]
+        }
+      });
+
+      this.map.addLayer({
+        id: 'building-point',
+        type: 'circle',
+        layout: {
+          visibility: "visible",
+        },
+        source: 'point',
+        paint: {
+          'circle-radius': [
+            'interpolate',
+            ['linear'],
+            ['zoom'],
+            8, 0,
+            9, 3,
+            10, 3.5,
+            11, 4,
+            12, 5,
+            13, 6,
+            14, 7,
+            15, 8,
+            16, 9,
+            17, 10
+          ],
+          'circle-color': 'blue'
+        }
+      });
+    });
+
+    this.map.on('mouseenter', 'building-point', () => {
+      this.map.getCanvas().classList.add('my-circle-class');
+    });
+
+    this.map.on('mouseleave', 'building-point', () => {
+      this.map.getCanvas().classList.remove('my-circle-class');
+    });
+
+
+
+
+    this.map.addControl(new ZoomControl(), 'top-right');
+    this.map.addControl(new LanguageControl({
+      language: 'ua'
+    }));
 
   }
 }
